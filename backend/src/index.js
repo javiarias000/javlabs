eb2ea5ca88c6c4af8f28fb11e5e4b17592eeb9c2
javiarias000 <javiarias000@gmail.com>
2026-03-18 21:08:30 +0000

    fix: bind server to 0.0.0.0 for docker

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,6 @@ const { logger } = require('./utils/logger');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   logger.info(`🚀 JAV LABS API corriendo en puerto ${PORT} [${process.env.NODE_ENV}]`);
 });
